1bb17c0881e49a66cfd4cabdc9f54d53c5fbbdbe
Joshua Sigona <sigonasr2@gmail.com>
2021-03-15 11:42:07 +0900

    Saki with typing

--- DiscordDivaBot/src/sig/Check.java
+++ DiscordDivaBot/src/sig/Check.java
@@ -1,6 +1,7 @@
 package sig;
 
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 import net.dv8tion.jda.api.JDA;
 
@@ -17,8 +18,9 @@ public class Check extends TimerTask{
 	public void run() {
 		if (!dead&&System.currentTimeMillis()-lastChannelMessage>=10800000) {
 			dead=true;
+			bot.getTextChannelById(772923108997857291l).sendTyping().queue();
 			bot.getTextChannelById(772923108997857291l).sendMessage("<:sakiDead1:799650259487293440><:sakiDead2:799650259746947083>")
-			.queue();
+			.queueAfter(1500,TimeUnit.MILLISECONDS);
 			/*bot.getTextChannelById(744692511703826462l).sendMessage("<:sakiPls:820748397031718923><:sakiPls:820748397031718923>")
 			.queue();*/
 		}
